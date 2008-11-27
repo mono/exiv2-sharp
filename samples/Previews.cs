8cdc726ca8304b94a90020b52ceb28201a4c26b8
Stephane Delcroix <stephane@mono-cvs.ximian.com>
2008-11-27 12:48:11 +0000

    2008-11-27  Stephane Delcroix  <sdelcroix@novell.com>
    	* new Previews sample
    
    svn path=/trunk/exiv2-sharp/; revision=120149

---
+++ samples/Previews.cs
@@ -0,0 +1,38 @@
+/*
+ * ExifComment.cs: c# port of http://www.exiv2.org/doc/exifcomment_8cpp-example.html
+ *
+ * Author(s):
+ *	Stephane Delcroix  <stephane@delcroix.org>
+ *
+ * This is free software. See COPYING for details
+ */
+
+using System;
+using Exiv2;
+
+namespace Sample
+{
+	public class ExifComment
+	{
+		static void Main (string [] args)
+		{
+			GLib.GType.Init ();
+			try {
+				if (args.Length != 1) {
+					Console.WriteLine ("Usage: Previews.exe file");
+					System.Environment.Exit (1);
+				}
+				Exiv2.Image image = Exiv2.ImageFactory.Open (args [0]);
+				image.ReadMetadata ();
+
+				PreviewManager manager = image.PreviewManager;
+				Console.WriteLine ("manager.PreviewProperties.Count = {0}", manager.PreviewProperties.Count);
+				foreach (PreviewProperties prop in manager.PreviewProperties) {
+					Console.WriteLine ("\t t:{0} e:{1} s:{2} w:{3} h:{4}", prop.MimeType, prop.Extension, prop.Size, prop.Width, prop.Height );
+				}
+			} catch (GLib.GException e) {
+				Console.WriteLine ("Exiv2.Exception caught {0}", e);
+			}
+		}
+	}
+}
